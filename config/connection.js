a7b2710076e5f2115cc4379a6ee5cc4891720972
sfoutz0205 <68661461+sfoutz0205@users.noreply.github.com>
2020-10-21 20:28:22 -0600

    user routes functioning

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,8 @@
 // import Sequelize constructor from the library
 const Sequelize = require('sequelize');
 
+require('dotenv').config();
+
 // create connection to our database
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
   host: 'localhost',
@@ -9,3 +11,6 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
 });
 
 module.exports = sequelize;
+
+
+
