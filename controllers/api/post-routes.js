6c00c3290e773f8431a6e2dc7baf4c8cc91d6b3e
sfoutz0205 <68661461+sfoutz0205@users.noreply.github.com>
2020-10-31 19:23:59 -0600

    single post page

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -92,12 +92,16 @@ router.post('/', (req, res) => {
 
 // VOTE FOR A POST
 router.put('/upvote', (req, res) => {
-  Post.upvote(req.body, { Vote })
+  // make sure the session exists first
+  if (req.session) {
+    // pass session id along with all destructured properties on req.body
+    Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
     .then(updatedPostData => res.json(updatedPostData))
     .catch(err => {
       console.log(err);
       res.status(400).json(err);
     });
+  }
 });
 
 
